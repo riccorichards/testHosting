d75c532b5a5022b67e960d7437839326e6fe57ff
Riccorichards <richard.tropimovi.1@iliauni.edu.ge>
2024-02-11 16:54:28 +0400

    vercel

--- src/api.ts
+++ src/api.ts
@@ -7,17 +7,6 @@ const api = (app: Application) => {
   const repo = new TestRepo(initializedRepo);
   const service = new ServiceTest(repo);
 
-  app.get("/", async (req: Request, res: Response) => {
-    try {
-      const image =
-        "https://i.pinimg.com/564x/cf/f4/2f/cff42f815b29f0d42d38e00e7f07ddba.jpg";
-
-      return res.redirect(image);
-    } catch (error) {
-      return res.status(500).json({ msg: "Error" });
-    }
-  });
-
   app.post("/create", async (req: Request, res: Response) => {
     try {
       const newTest = await service.CreateTestService(req.body);
